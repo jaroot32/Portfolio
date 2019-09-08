6c28c570e2b08ad25b94b8425e2f1144fb62d24f
Jonathan Root <jonathanmbp2019@Jonathans-MacBook-Pro.local>
2019-09-07 19:44:49 -0700

    added manifest image and udacity card

--- config/site.js
+++ config/site.js
@@ -5,7 +5,7 @@ module.exports = {
   siteTitleAlt: 'Jonathan Root -  Project Portfolio', // Alternative Site title for SEO
   siteTitleShort: 'Jonathan Root', // short_name for manifest
   siteHeadline: 'Examples of my work as a Developer', // Headline for schema.org JSONLD
-  siteUrl: 'https://ecstatic-leakey-76d0ff.netlify.com/', // Domain of your site. No trailing slash!
+  siteUrl: 'https://emilia.lekoarts.de', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.svg', // Used for SEO and manifest
   siteDescription: 'Examples of my work as a Developer, Instructor, Mentor, and Designer',
@@ -13,7 +13,7 @@ module.exports = {
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   ogLanguage: 'en_US', // og:language
-  // googleAnalyticsID: 'UA-47519312-4',
+  googleAnalyticsID: 'UA-47519312-4',
 
   // Manifest and Progress color
   themeColor: '#3498DB',
